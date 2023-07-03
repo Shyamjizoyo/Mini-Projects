08b9bf6ff63fbe48abfea3431518ebc985fc1987
Shyam Ji Gupta <91778264+Shyamjizoyo@users.noreply.github.com>
2023-07-03 23:57:30 +0530

    Add files via upload

---
+++ Joke Genrator/script.js
@@ -0,0 +1,31 @@
+const jokeArea = document.querySelector("#joke-area");
+const jokeButton = document.querySelector("#joke-button");
+// const url = "https://v2.jokeapi.dev/joke/Any";
+const url = "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky";
+
+jokeButton.addEventListener("click", (e) => {
+  jokeArea.classList.add("loading");
+  loadingTimeout();
+  generateJokes();
+});
+
+async function generateJokes() {
+  const res = await fetch(url);
+  const data = await res.json();
+  jokeArea.innerText = "";
+  let joke = "";
+  if (data.joke == undefined) {
+    joke = `${data.setup} ${data.delivery}`;
+  } else {
+    joke = data.joke;
+  }
+  jokeArea.innerText = joke;
+}
+
+function loadingTimeout() {
+  setTimeout(() => {
+    jokeArea.classList.remove("loading");
+    joke.style.color = "black";
+    jokeArea.style.color = "transparent";
+  }, 3000);
+}
